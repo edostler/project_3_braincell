33c00aaead1450497058763df327c010fca7ec0e
Ed Ostler <edward.o@simul8.com>
2018-04-27 17:12:41 +0100

    Adds game container constructor states

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -5,6 +5,23 @@ import MiddleContainer from './MiddleContainer';
 
 class GameContainer extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      gameStatus: 0,
+      // gameStatus: 0 = Start, 1 = Choose Category, 2 = In Play, 3 = End
+      playerName: null,
+      allCategories: [],
+      playerCategories: [],
+      questions: [[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]]],
+      currentQuestion: [],
+      currentCell: 0,
+      currentPoints: 0,
+      currentDifficulty: 1
+    }
+    this.handleMove = this.handleMove.bind(this);
+  }
+
   render(){
     return (
       <React.Fragment>
