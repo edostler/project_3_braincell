d28e6b8f940acb618ca04048e82e128f240aec70
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-27 19:34:47 +0100

    Changed MiddleContainer to be a stateless function. Passed down allCategories and playerCategories to MiddleContainer, and then onto CategoryContainer, so they can be used to drive behaviour of the CategoryContainer.

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -11,8 +11,8 @@ class GameContainer extends Component {
       gameStatus: 0,
       // gameStatus: 0 = Start, 1 = Choose Category, 2 = In Play, 3 = End
       playerName: null,
-      allCategories: [],
-      playerCategories: [],
+      allCategories: [1,2,3,4,5,6],         // Hard coded until we have API data
+      playerCategories: [1,2,3,4,5,6],      // Hard coded until we have API data
       questions: [[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]]],
       currentQuestion: [],
       currentCell: 0,
@@ -23,7 +23,7 @@ class GameContainer extends Component {
   }
 
   handleMove(){
-    
+
   }
 
   render(){
@@ -31,7 +31,10 @@ class GameContainer extends Component {
       <React.Fragment>
         <p>I'm a Game Container</p>
         <CellContainer/>
-        <MiddleContainer/>
+        <MiddleContainer
+          allCategories={this.state.allCategories}
+          playerCategories={this.state.playerCategories}
+        />
       </React.Fragment>
     )
   }
--- src/containers/MiddleContainer.js
+++ src/containers/MiddleContainer.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './MiddleContainer.css';
 import Start from '../components/Start';
 import QuizContainer from './QuizContainer';
 import CategoryContainer from './CategoryContainer';
 import End from '../components/End';
 
-class MiddleContainer extends Component {
+const MiddleContainer = (props) => {
 
-  render(){
+    if(!props.allCategories || !props.playerCategories){
+      return null;
+    }
+
     return (
       <React.Fragment>
         <p>I'm a Middle Container</p>
         <Start/>
-        <CategoryContainer />
+        <CategoryContainer
+          allCategories={props.allCategories}
+          playerCategories={props.playerCategories}
+        />
         <QuizContainer/>
         <End/>
       </React.Fragment>
     )
-  }
+
 
 }
 
