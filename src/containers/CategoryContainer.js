be5ee77ceb2e293bcca92ff1558484f7669eb38c
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-27 17:09:14 +0100

    Added Category container and component

---
+++ src/containers/CategoryContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './CategoryContainer.css';
+import Category from '../components/Category';
+
+
+class CategoryContainer extends Component {
+
+  render(){
+    return (
+      <Category />
+    )
+  }
+
+}
+
+export default CategoryContainer;
--- src/containers/MiddleContainer.js
+++ src/containers/MiddleContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './MiddleContainer.css';
 import Start from '../components/Start';
 import QuizContainer from './QuizContainer';
+import CategoryContainer from './CategoryContainer';
 import End from '../components/End';
 
 class MiddleContainer extends Component {
@@ -11,6 +12,7 @@ class MiddleContainer extends Component {
       <React.Fragment>
         <p>I'm a Middle Container</p>
         <Start/>
+        <CategoryContainer />
         <QuizContainer/>
         <End/>
       </React.Fragment>
