c65a0475f9a531a7e87dc73c753f6f0e77abdb6b
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-27 17:20:19 +0100

    adds api url builder

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -7,67 +7,68 @@ class GameContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
+      allQuestions: [],
       questions: [],
       categories: [
         {id: 9, name: "General Knowledge", state: 1},
-        {id: 17, name: "Science & Nature", state: 1}
-        {id: 20, name: "Mythology", state: 1}
-        {id: 21, name: "Sports", state: 1}
-        {id: 22, name: "Geography", state: 1}
-        {id: 23, name: "History", state: 1}
-        {id: 24, name: "Politics", state: 1}
-        {id: 25, name: "Art", state: 1}
+        {id: 17, name: "Science & Nature", state: 1},
+        {id: 20, name: "Mythology", state: 1},
+        {id: 21, name: "Sports", state: 1},
+        {id: 22, name: "Geography", state: 1},
+        {id: 23, name: "History", state: 1},
+        {id: 24, name: "Politics", state: 1},
+        {id: 25, name: "Art", state: 1},
         {id: 27, name: "Animals", state: 1}
-      ]
+      ],
+      difficulties: ["easy", "medium", "hard"]
     };
     // this.loadJokes = this.loadJokes.bind(this);
     // this.handleCategorySelected = this.handleCategorySelected.bind(this);
     // this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
+  // difficulty = easy, medium, hard
+  //
+  // type = multiple
+  //
+  // https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple
+
+  // const jokeList = this.props.jokes.map((joke, index) => {
+  //   return(
+  //     <Joke key={index} joke={joke.joke}></Joke>
+  //   )
+  // })
+
   componentDidMount(){
-    fetch("http://api.icndb.com/categories")
-    .then(response => response.json())
-    .then(json => this.setState({categories:json.value}))
-    .then(this.loadRandomJoke(null, null))
-    this.loadJokes("all")
+// debugger;
+// loop thru this.state.categories
+    const gameCategories = this.state.categories.map((category, index) => {
+// where state is 1
+      if (this.state.categories[index].state === 1){
+        // console.log(this.state.categories[index].name);
+// use id (this.state.categories[index].id)
+// loop thru for each difficulty
+        const gameDifficulty = this.state.difficulties.map(difficulty => {
+          // console.log("difficult:", difficulty);
+  // fetch from API
+          let url = "https://opentdb.com/api.php?amount=10&category=" +
+            this.state.categories[index].id +
+            "&difficulty=" +
+            difficulty +
+            "&type=multiple";
+            // console.log(url);
+            fetch(url)
+// write content to state
+            .then(response => response.json())
+            .then(json => this.setState({questions:json.results}))
+            // console.log(this.state.questions);
+            // this.setState({allQuestions: this.state.allQuestions + this.state.questions});
+            // console.log(this.state.allQuestions);
+        })
+      }
+    })
   }
 
-  loadRandomJoke(newFirstName, newLastName){
-    let url;
-    if(!newFirstName || !newLastName){
-      url = "http://api.icndb.com/jokes/random/"
-    } else {
-      url = "http://api.icndb.com/jokes/random/?firstName=" + newFirstName + "&lastName=" + newLastName;
-    }
-    fetch(url)
-    .then(response => response.json())
-    .then(json => this.setState({randomJoke: json.value}))
-  }
-
-  loadJokes(category, newFirstName, newLastName){
-    let url;
-    if(!newFirstName || !newLastName){
-      switch(category){
-        case "all":
-        url = "http://api.icndb.com/jokes";
-        break;
-        default:
-        url = "http://api.icndb.com/jokes?limitTo=[" + category + "]";
-      }
-    } else {
-      switch(category){
-        case "all":
-        url = "http://api.icndb.com/jokes?firstName=" + newFirstName + "&lastName=" + newLastName;
-        break;
-        default:
-        url = "http://api.icndb.com/jokes?limitTo=[" + category + "]&firstName=" + newFirstName + "&lastName=" + newLastName;
-      }
-    }
-    fetch(url)
-    .then(response => response.json())
-    .then(json => this.setState({jokes: json.value}));
-  }
 
   render(){
     return (
