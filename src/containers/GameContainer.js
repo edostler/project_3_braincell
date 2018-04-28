e12847bd967d7285b4cdabbea72a46623bf79f5e
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-28 10:34:44 +0100

    api builder not working as expected

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -11,64 +11,59 @@ class GameContainer extends Component {
       questions: [],
       categories: [
         {id: 9, name: "General Knowledge", state: 1},
-        {id: 17, name: "Science & Nature", state: 1},
-        {id: 20, name: "Mythology", state: 1},
-        {id: 21, name: "Sports", state: 1},
-        {id: 22, name: "Geography", state: 1},
-        {id: 23, name: "History", state: 1},
-        {id: 24, name: "Politics", state: 1},
-        {id: 25, name: "Art", state: 1},
-        {id: 27, name: "Animals", state: 1}
+        {id: 17, name: "Science & Nature", state: 0},
+        {id: 20, name: "Mythology", state: 0},
+        {id: 21, name: "Sports", state: 0},
+        {id: 22, name: "Geography", state: 0},
+        {id: 23, name: "History", state: 0},
+        {id: 24, name: "Politics", state: 0},
+        {id: 25, name: "Art", state: 0},
+        {id: 27, name: "Animals", state: 0}
       ],
-      difficulties: ["easy", "medium", "hard"]
+      // difficulties: ["easy", "medium", "hard"]
+      difficulties: ["easy"]
     };
     // this.loadJokes = this.loadJokes.bind(this);
     // this.handleCategorySelected = this.handleCategorySelected.bind(this);
     // this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  // difficulty = easy, medium, hard
-  //
-  // type = multiple
-  //
-  // https://opentdb.com/api.php?amount=50&category=9&difficulty=medium&type=multiple
-
-  // const jokeList = this.props.jokes.map((joke, index) => {
-  //   return(
-  //     <Joke key={index} joke={joke.joke}></Joke>
-  //   )
-  // })
-
   componentDidMount(){
-// debugger;
-// loop thru this.state.categories
-    const gameCategories = this.state.categories.map((category, index) => {
-// where state is 1
+    let batchQuestions = [];
+    // let allQuestions = [];
+    const questionCategories = this.state.categories.map((category, index) => {
       if (this.state.categories[index].state === 1){
-        // console.log(this.state.categories[index].name);
-// use id (this.state.categories[index].id)
-// loop thru for each difficulty
-        const gameDifficulty = this.state.difficulties.map(difficulty => {
-          // console.log("difficult:", difficulty);
-  // fetch from API
+        const questionDifficulty = this.state.difficulties.map(difficulty => {
           let url = "https://opentdb.com/api.php?amount=10&category=" +
             this.state.categories[index].id +
             "&difficulty=" +
             difficulty +
             "&type=multiple";
-            // console.log(url);
-            fetch(url)
+// console.log(this.state);
+          fetch(url)
 // write content to state
-            .then(response => response.json())
-            .then(json => this.setState({questions:json.results}))
-            // console.log(this.state.questions);
-            // this.setState({allQuestions: this.state.allQuestions + this.state.questions});
-            // console.log(this.state.allQuestions);
+          .then(response => response.json())
+          // .then(json => this.setState({questions:json.results}))
+          // .then(json => this.setState(() => {
+          //   return {questions: json.results};
+          // }))
+          // .then((json) => (batchQuestions.push(json.results)))
+          .then((json) => (batchQuestions => (json.results)))
+          // debugger;
+console.log(batchQuestions);
+
+
+          // let latestQuestions = this.state.questions;
+          // console.log("latestQuestions", this.state.questions, latestQuestions);
+          // let allQuestions = this.state.allQuestions;
+          // allQuestions += latestQuestions;
+          // debugger;
+          // this.setState({allQuestions: allQuestions});
+// console.log(this.state);
         })
       }
     })
   }
-
 
   render(){
     return (
