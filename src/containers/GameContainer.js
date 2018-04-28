7a14f3067aa7a927aabf41380a74cbd0ffe07542
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-28 11:21:00 +0100

    api builder working as expected - further work needed for setState

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -7,21 +7,20 @@ class GameContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-      allQuestions: [],
       questions: [],
       categories: [
         {id: 9, name: "General Knowledge", state: 1},
-        {id: 17, name: "Science & Nature", state: 0},
-        {id: 20, name: "Mythology", state: 0},
-        {id: 21, name: "Sports", state: 0},
-        {id: 22, name: "Geography", state: 0},
-        {id: 23, name: "History", state: 0},
-        {id: 24, name: "Politics", state: 0},
-        {id: 25, name: "Art", state: 0},
-        {id: 27, name: "Animals", state: 0}
+        {id: 17, name: "Science & Nature", state: 1},
+        {id: 20, name: "Mythology", state: 1},
+        {id: 21, name: "Sports", state: 1},
+        {id: 22, name: "Geography", state: 1},
+        {id: 23, name: "History", state: 1},
+        {id: 24, name: "Politics", state: 1},
+        {id: 25, name: "Art", state: 1},
+        {id: 27, name: "Animals", state: 1}
       ],
-      // difficulties: ["easy", "medium", "hard"]
-      difficulties: ["easy"]
+      difficulties: ["easy", "medium", "hard"]
+      // difficulties: ["easy"]
     };
     // this.loadJokes = this.loadJokes.bind(this);
     // this.handleCategorySelected = this.handleCategorySelected.bind(this);
@@ -29,8 +28,7 @@ class GameContainer extends Component {
   }
 
   componentDidMount(){
-    let batchQuestions = [];
-    // let allQuestions = [];
+    let allQuestions = [];
     const questionCategories = this.state.categories.map((category, index) => {
       if (this.state.categories[index].state === 1){
         const questionDifficulty = this.state.difficulties.map(difficulty => {
@@ -39,30 +37,15 @@ class GameContainer extends Component {
             "&difficulty=" +
             difficulty +
             "&type=multiple";
-// console.log(this.state);
           fetch(url)
-// write content to state
           .then(response => response.json())
-          // .then(json => this.setState({questions:json.results}))
-          // .then(json => this.setState(() => {
-          //   return {questions: json.results};
-          // }))
-          // .then((json) => (batchQuestions.push(json.results)))
-          .then((json) => (batchQuestions => (json.results)))
-          // debugger;
-console.log(batchQuestions);
-
-
-          // let latestQuestions = this.state.questions;
-          // console.log("latestQuestions", this.state.questions, latestQuestions);
-          // let allQuestions = this.state.allQuestions;
-          // allQuestions += latestQuestions;
-          // debugger;
-          // this.setState({allQuestions: allQuestions});
-// console.log(this.state);
+          .then((json) => (allQuestions.push(json.results)))
         })
       }
     })
+    console.log("All Questions", allQuestions);
+this.setState({questions: allQuestions})
+    // this.state.questions = allQuestions;
   }
 
   render(){
