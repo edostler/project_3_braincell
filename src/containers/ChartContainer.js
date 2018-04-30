fb5690f18cfb2d162d437200dfd60ad8edbc2eee
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-30 16:46:41 +0100

    Added skeleton ChartContainer and CSS

---
+++ src/containers/ChartContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ChartContainer.css';
+
+const ChartContainer = (props) => {
+
+    return (
+      <React.Fragment>
+        <div className="chart-container">
+          <p>I'm a Chart Container</p>
+        </div>
+      </React.Fragment>
+    )
+
+}
+
+export default ChartContainer;
