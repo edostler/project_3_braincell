e687cfa37993dc5a03e8b2d6c1b8715e0f68322c
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-30 18:38:45 +0100

    Added constructor to App.js, with a state property 'showChart' that can be used to drive the visibility of the chart in the GameContainer area - using z-index - by changing the z-index from -2 to 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import GameContainer from './containers/GameContainer';
+import ChartContainer from './containers/ChartContainer';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      showChart: 0
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <Header
+          showChart={this.state.showChart}
           // playerName={this.state.playerName}
           // currentPoints={this.state.currentPoints}
+        />
+        <ChartContainer
+          showChart={this.state.showChart}
         />
         <GameContainer/>
         <Footer/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,7 @@ const Header = (props) =>{
         {/* <p>You have earned {props.currentPoints} points, well done!!</p> */}
         <h1 className="header-title">Hello, welcome to BrainCell!</h1>
         <img className="header-logo" src="BrainCell_Framed.png" alt=""/>
+        <p className="chart-status">Show Chart Status: {props.showChart}</p>
         <img className="chart-icon" src="chart-icon.png" alt=""/>
       </React.Fragment>
     );
