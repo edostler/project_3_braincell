e75909f012687a6eaa982259dd4e61c061c0e16e
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-30 10:08:46 +0100

    api builder working as expected

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -16,23 +16,33 @@ class GameContainer extends Component {
         {id: 22, name: "Geography", state: 1},
         {id: 23, name: "History", state: 1},
         {id: 24, name: "Politics", state: 1},
-        {id: 25, name: "Art", state: 1},
+        {id: 25, name: "Art", state: 0},
         {id: 27, name: "Animals", state: 1}
       ],
       difficulties: ["easy", "medium", "hard"]
       // difficulties: ["easy"]
     };
-    // this.loadJokes = this.loadJokes.bind(this);
+    this.getQuestionCount = this.getQuestionCount.bind(this);
     // this.handleCategorySelected = this.handleCategorySelected.bind(this);
     // this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
   componentDidMount(){
     let allQuestions = [];
+    let defaultQuestionCount = 5
     const questionCategories = this.state.categories.map((category, index) => {
       if (this.state.categories[index].state === 1){
+        let easyQCount = 0;
+        let medQCount = 0;
+        let hardQCount = 0;
+        // console.log(category.id);
+        // this.getQuestionCount(category.id);
+        // console.log("Easy:", easyQCount, "Medium:", medQCount, "Hard:", hardQCount);
+        // console.log(easyQCount);
         const questionDifficulty = this.state.difficulties.map(difficulty => {
-          let url = "https://opentdb.com/api.php?amount=10&category=" +
+          let url = "https://opentdb.com/api.php?amount=" +
+            defaultQuestionCount +
+            "&category=" +
             this.state.categories[index].id +
             "&difficulty=" +
             difficulty +
@@ -40,12 +50,34 @@ class GameContainer extends Component {
           fetch(url)
           .then(response => response.json())
           .then((json) => (allQuestions.push(json.results)))
+          // .then(blah =>
+          // this.setState({questions: allQuestions}));
         })
       }
     })
     console.log("All Questions", allQuestions);
-this.setState({questions: allQuestions})
-    // this.state.questions = allQuestions;
+    // this.setState({questions: allQuestions})
+    this.state.questions = allQuestions;
+    console.log("this State", this.state.questions);
+  }
+
+  getQuestionCount(catId, easyQuestionCount, mediumQuestionCount, hardQuestionCount){
+    // console.log("cat id: ", catId, " diff: ", diff);
+    let questionCount = [];
+    let questionCountUrl = "https://opentdb.com/api_count.php?category=" +
+      catId;
+    // console.log(questionCountUrl);
+    fetch (questionCountUrl)
+    .then(response => response.json())
+    .then((json) => {
+      questionCount.push(json.category_question_count)
+      // console.log("Question Count:", questionCount[0]);
+      easyQuestionCount = questionCount[0].total_easy_question_count;
+      mediumQuestionCount = questionCount[0].total_medium_question_count;
+      hardQuestionCount = questionCount[0].total_hard_question_count;
+      console.log("Cat:", catId, "Easy:", easyQuestionCount, "Medium:", mediumQuestionCount, "Hard:", hardQuestionCount);
+      return (easyQuestionCount, mediumQuestionCount, hardQuestionCount)
+    });
   }
 
   render(){
