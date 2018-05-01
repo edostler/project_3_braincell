8a48141bca9b633ca99e333c40a046531f236eac
Ed Ostler <edward.o@simul8.com>
2018-05-02 00:14:21 +0100

    adds outline to cells to show current space

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,14 +3,30 @@ import './Cell.css';
 
 const Cell = (props) => {
 
-  return (
-    <article id={props.difficulty} className="box">
-      <div id="inner" className={props.cellImages[parseInt(props.position - 1, 10)]}>
-        <figure className="unanswered">Cell {props.position}</figure>
-        <figure className="answered">Cell {props.position}</figure>
-      </div>
-    </article>
-  )
+  var figureStyle = {
+    outline: "2px solid black"
+  };
+
+  if((parseInt(props.position, 10) === props.currentCell + 1) && props.gameStatus > 0) {
+    return (
+      <article id={props.difficulty} className="box">
+        <div id="inner" className={props.cellImages[parseInt(props.position - 1, 10)]}>
+          <figure style={figureStyle} className="unanswered">Cell {props.position}</figure>
+          <figure className="answered">Cell {props.position}</figure>
+        </div>
+      </article>
+    )
+  }
+  else {
+    return (
+      <article id={props.difficulty} className="box">
+        <div id="inner" className={props.cellImages[parseInt(props.position - 1, 10)]}>
+          <figure className="unanswered">Cell {props.position}</figure>
+          <figure className="answered">Cell {props.position}</figure>
+        </div>
+      </article>
+    )
+  }
 
 }
 
--- src/containers/CellContainer.js
+++ src/containers/CellContainer.js
@@ -8,52 +8,52 @@ const CellContainer = (props) => {
   return (
     <article className="grid">
       <div className="row">
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="5"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="6"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="7"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="8"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="9"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="10"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="5"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="6"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="7"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="8"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="9"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="medium" position="10"/>
       </div>
       <div className="row">
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="4"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="4"/>
         <BlankCell />
         <BlankCell />
         <BlankCell />
         <BlankCell />
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="11"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="11"/>
       </div>
       <div className="row">
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="3"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="3"/>
         <BlankCell />
         <BlankCell />
         <BlankCell />
         <BlankCell />
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="12"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="12"/>
       </div>
       <div className="row">
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="2"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="2"/>
         <BlankCell />
         <BlankCell />
         <BlankCell />
         <BlankCell />
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="13"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="13"/>
       </div>
       <div className="row">
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="1"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="easy" position="1"/>
         <BlankCell />
         <BlankCell />
         <BlankCell />
         <BlankCell />
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="14"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="14"/>
       </div>
       <div className="row">
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="20"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="19"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="18"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="17"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="16"/>
-        <Cell cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="15"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="20"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="19"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="18"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="17"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="mix" position="16"/>
+        <Cell gameStatus={props.gameStatus} cellImages={props.cellImages} currentCell={props.currentCell} difficulty="hard" position="15"/>
       </div>
     </article>
   )
