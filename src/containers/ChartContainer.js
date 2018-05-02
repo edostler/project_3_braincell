7fdd00a4e277e917b7a90afc7a3db5b161c2698e
Sarah Campbell <scampbell.home@btinternet.com>
2018-05-02 16:14:45 +0100

    Added a column chart

--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './ChartContainer.css';
 
+import { render } from 'react-dom';
+import { Chart } from 'react-google-charts';
 
 
 class ChartContainer extends Component {
@@ -18,17 +20,35 @@ class ChartContainer extends Component {
     .then(response => response.json())
     .then ((json) => {
       this.setState({playerData: (json)})
+      // this.state.playerData = (json)
       console.log("this",this.state.playerData)
     })
   }
 
+
   render(){
+    if (!this.state.playerData) return null;
     return (
-      <React.Fragment>
         <div className="chart-container" style={{zIndex: this.props.showChart}}>
           <p>I'm a Chart Container</p>
+          <Chart
+            chartType="ColumnChart"
+            data={[['Name', 'Score'],
+                  // [this.state.playerData[9].name, this.state.playerData[9].result],
+                  // [this.state.playerData[10].name, this.state.playerData[10].result],
+                  // [this.state.playerData[11].name, this.state.playerData[11].result]
+                  ["Ed", 1000],
+                  ["Debbie", 980],
+                  ["Graeme",900],
+                  ["Sarah", 800]
+                ]}
+            options={{}}
+            graph_id="ColumnChart"
+            width="100%"
+            height="400px"
+            legend_toggle
+          />
         </div>
-      </React.Fragment>
     )
   }
 
