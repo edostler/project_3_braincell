510df871b0a4cd2ad983b1657aea1c2dca993263
Sarah Campbell <scampbell.home@btinternet.com>
2018-05-02 15:06:04 +0100

    Changed chart container to be a state component (class) and fetched our player data from our local server

--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ChartContainer.css';
 
-const ChartContainer = (props) => {
 
+
+class ChartContainer extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      playerData: []
+    };
+  }
+
+  async componentDidMount(){
+    let url = "http://localhost:3001/players";
+    fetch(url)
+    .then(response => response.json())
+    .then ((json) => {
+      this.setState({playerData: (json)})
+      console.log("this",this.state.playerData)
+    })
+  }
+
+  render(){
     return (
       <React.Fragment>
-        <div className="chart-container" style={{zIndex: props.showChart}}>
+        <div className="chart-container" style={{zIndex: this.props.showChart}}>
           <p>I'm a Chart Container</p>
         </div>
       </React.Fragment>
     )
+  }
 
 }
+
 
 export default ChartContainer;
